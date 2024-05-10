3ca4a6b2b10d92f16320255e8cd89d3d90ba9273
Kunal7069 <jaink7069@gmail.com>
2024-05-10 12:04:08 +0530

    API WORKING

--- fitness/fitness/views.py
+++ fitness/fitness/views.py
@@ -7,17 +7,21 @@ import requests
 from rest_framework.generics import ListAPIView
 
 class NutritionEstimateView(ListAPIView):
-    def get(self, request, *args, **kwargs):
-        food_item = request.query_params.get("food")
+    def post(self, request, *args, **kwargs):
+        data=request.data
+        food_item=data['food']
+        print(data['food'])
         url = f'https://api.edamam.com/api/nutrition-data?app_id=21b4ee87&app_key=a0a287ed2537403d3c5b4a4d89e14091&nutrition-type=cooking&ingr={food_item}'
         response = requests.get(url).json()
-        return HttpResponse(response)
+        return Response(response)
 
 class QuantityEstimateView(ListAPIView):
-    def get(self, request, *args, **kwargs):
-        food_item = "1 kg "+str(request.query_params.get("food"))
-        nutrient = request.query_params.get("nutrient")
-        required_nutrient_quantity = int(request.query_params.get("required_quantity"))
+    def post(self, request, *args, **kwargs):
+        data=request.data
+        food=data['food']
+        nutrient = data['nutrient']
+        required_nutrient_quantity = data['required_nutrient_quantity']
+        food_item = "1 kg "+str(food)
         url = f'https://api.edamam.com/api/nutrition-data?app_id=21b4ee87&app_key=a0a287ed2537403d3c5b4a4d89e14091&nutrition-type=cooking&ingr={food_item}'
         response = requests.get(url).json()
         return HttpResponse(str(required_nutrient_quantity/response['totalNutrients'][nutrient]['quantity']) +' kg')
@@ -62,12 +66,13 @@ class IndigreintsWiseRecipeView(ListAPIView):
         }
 
         response = requests.get(url, headers=headers, params=querystring)
-        recipe_name=[]
-        for i in response.json():
-            recipe_name.append(i['title'])
-        print(recipe_name)
+        return Response(response)
+        # recipe_name=[]
+        # for i in response.json():
+        #     recipe_name.append(i['title'])
+        # print(recipe_name)
 
-        return Response(recipe_name)
+        # return Response(recipe_name)
 
 def home(request):
     return render(request,"home.html")    
